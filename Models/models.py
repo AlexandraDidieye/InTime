432909c6b7b54d6ca3df8e5c3241bac7228cbb81
lexynovicecoder <didieyealexandra@gmail.com>
2025-10-05 16:46:03 +0000

    established relationships between models

--- Models/models.py
+++ Models/models.py
@@ -8,6 +8,8 @@ class User(SQLModel, table=True):
     email: str = Field(sa_column=Column("email", String, unique=True))
     first_name: str
     last_name:str
+    subjects: List["Subject"] = Relationship(back_populates="user",sa_relationship_kwargs={"cascade": "all, delete-orphan"})
+    assignments: List["Assignment"] = Relationship(back_populates="user")
 
 class Subject(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
@@ -15,8 +17,12 @@ class Subject(SQLModel, table=True):
     progress: Optional[int] = Field(default=0)
     total_assignments: Optional[int] = Field(default=0)  # Default value
     completed_assignments: Optional[int] = Field(default=0)
+    assignments: List["Assignment"] = Relationship(back_populates="subject",sa_relationship_kwargs={"cascade": "all, delete-orphan"})
+    user_id: int = Field(default=None, foreign_key="user.id")
+    user: Optional[User] = Relationship(back_populates="subjects")
 
-class Assignments(SQLModel, table=True):
+
+class Assignment(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
     name: str = Field(index=True)
     is_completed: bool = Field(default=False)  # Default value for is_completed
@@ -28,3 +34,8 @@ class Assignments(SQLModel, table=True):
     priority: str = Field(index=True)
     number_of_questions: Optional[int] = Field(default=0)
     number_of_completed_questions: Optional[int] = Field(default=0)
+    subject_id: int = Field(default=None,foreign_key="subject.id")
+    subject: Optional[Subject] = Relationship(back_populates="assignments")
+    user_id : int = Field(default=None, foreign_key="user.id")
+    user: Optional[User] = Relationship(back_populates="assignments")
+
